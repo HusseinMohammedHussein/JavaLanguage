28babca50375a028496e5f3c3f9938c4b71feaa9
Hussein Mohammed Hussein <Husseinsaqr43@gmail.com>
2020-04-08 12:29:55 +0200

    Update TestTypesOfJavaVariables.java

--- TestTypesOfJavaVariables.java
+++ TestTypesOfJavaVariables.java
@@ -7,21 +7,21 @@ class VarEx{
         	int var2 = 20;
 
 	public static void main(String[] args) {
-				/* Run Static Varyable */ 
-				System.out.println(var1);
-				/* Run the Instanc Varyable */ 
-				VarEx v = new VarEx();
-				System.out.println(v.var2);
-				/* Try run [Method] Outside [MAIN_Method] */ 
-				externalMethod();
+		/* Run Static Varyable */ 
+		System.out.println(var1);
+		/* Run the Instanc Varyable */ 
+		VarEx v = new VarEx();
+		System.out.println(v.var2);
+		/* Try run [Method] Outside [MAIN_Method] */ 
+		externalMethod();
 
 	}
 
 	/* External Method */ 
 	public static  void externalMethod(){
-			final int var3 = 30;
-			System.out.println(var3);
-		}
+		final int var3 = 30;
+		System.out.println(var3);
+	}
 }		
 
 class VarEx2{
